3924156c31bbd1b3d444d4afa67a201f4d384ca0
Trần Đăng Khoa <khoatranpc603@gmail.com>
2022-04-02 21:45:55 +0700

    lesson1

---
+++ Pratice1/index.js
@@ -0,0 +1,70 @@
+// cách đặt biến
+
+// let nameUser = "Chi pheo"
+
+// nameUser = "Dep trai"
+//kiểu dữ liệu, ở trong js: string, number, object, array
+
+// let a = "Sasuke";
+
+// let b = 'Naruto';
+
+// let c = `Yamato ${a}`;
+
+// let a = 4;
+
+// let b = 2;
+
+// console.log((a%b));
+
+// điều kiện
+// có câu lệnh: if, if else, if (else if) else
+// so sánh: < >  <=  >= ==  !=
+// let age = 10;
+
+// if(nameUser== "Chi pheo"){
+//     console.log("đây là chí phèo");
+// }
+
+// if(nameUser != "Chi pheo"){
+//     console.log("Không phải là chí phèo");
+// }else{
+//     console.log("Đây là chí phèo");
+// }
+// 10 10
+
+// if(age > 10 || age == 11){
+//     console.log("Đây là thanh niên");
+// }else if(age === "10"){
+//     console.log("Đây là thiếu nhi");
+// }else{
+//     console.log("Đây là trẻ con");
+// }
+
+// điều kiện với switch case
+// let m = 10;
+
+// switch (m) {
+//     case 10:
+//         console.log("Đây là tháng 10");
+//         break;
+//     case 9:
+//         console.log("Đây là tháng 9");
+//         break;
+//     case 8:
+//         console.log("Đây là tháng 8");
+//         break;
+//     default:
+//         console.log("Không phải tháng trong năm");
+//         break;
+// }
+
+// Kiểm tra 1 số có phải là số chính phương hay không
+
+// Viết ra số nhị phân của một số nhập vào
+
+// kiểm tra só chẵn, số lẽ
+
+// viết ra căn bậc 2 của 1 số, bình phương, lập phương,...
+
+// buổi sau: về vòng lặp (loop), array, object
